2a7a411414b72b7a5480a7484f933756d977f609
devluisluzardo <devluisluzardo@gmail.com>
2024-09-02 11:25:17 +0000

    first commit ini

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./routes/Home";
+import Login from "./routes/Login";
+import NotFound from "./routes/NotFound";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+
+      {/* 
+        <h1>App Encab</h1> 
+      */}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
